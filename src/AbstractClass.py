c1581aead700227ee1ba76002da957f41eaf4f65
Mikhail <osipov.m93@mail.ru>
2023-07-09 14:45:07 +0300

    add AbstractClass

---
+++ src/AbstractClass.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractAPIVacancy(ABC):
+
+    # @abstractmethod
+    # def get_vacancies(self, job_title):
+    #     pass
+
+    @staticmethod
+    def add_vacancy(obj):
+        pass
+
+    @staticmethod
+    def remove_vacancy(vacancy_del):
+        pass
+
+    @staticmethod
+    def top_salary_vacancy(obj):
+        pass
